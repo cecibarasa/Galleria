fdf0ccbf5233926d967e278f34153809eb72f23d
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-25 16:24:26 +0300

    photographer model class

--- album/models.py
+++ album/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Photographer(models.Model):
+    first_name = models.CharField(max_lengh=20)
+    second_name = models.CharField(max_lengh=20)
+    email = models.EmailField()
