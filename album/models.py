e776ff76ca23af8f3058692e83c698df680018c9
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-25 16:44:39 +0300

    add tag model class

--- album/models.py
+++ album/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Photographer(models.Model):
-    first_name = models.CharField(max_lengh=20)
-    second_name = models.CharField(max_lengh=20)
+    first_name = models.CharField(max_length=20)
+    second_name = models.CharField(max_length=20)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+class tag(models.Model):
+    category = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category        
