ffbbffee2dd40b6e317425e3dfded3e7c646182c
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-25 15:28:06 +0300

    add datetime

--- album/urls.py
+++ album/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^$',views.welcome,name = 'welcome'),
+    url('^$', views.welcome, name='welcome'),
+    uurl('^today/$',views.picture_of_day,name='albumToday'))
 ]
