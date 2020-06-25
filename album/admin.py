308f9af6b70bc6a056f22096e6a35d986de5fe1b
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-25 18:13:11 +0300

    add admin models

--- album/admin.py
+++ album/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Photographer,Photos,Location,Category,tag
 
 # Register your models here.
+admin.site.register(Photographer)
+admin.site.register(Photos)
+admin.site.register(Photographer)
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(tag)
